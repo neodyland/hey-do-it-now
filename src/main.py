dad2330763ec9893e4db0f437d8b21838fc08750
コマリン親衛隊 <masato190411@icloud.com>
2024-01-26 15:26:06 +0000

    inital commit

---
+++ src/main.py
@@ -0,0 +1,15 @@
+import dotenv
+import requests
+from os import getenv
+
+
+def main() -> None:
+    dotenv.load_dotenv()
+    r = requests.post(getenv("DISCORD_WEBHOOK_URI"), json={
+        "content": "<@{userid}> さっさと働け！".format(userid=getenv("TARGET_ID"))
+    })
+    r.raise_for_status()
+
+
+if __name__ == "__main__":
+    main()
